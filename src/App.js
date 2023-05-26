a68b492e3306aa7b7994be5f1895ad68b584dfc7
i-zakiro <90312517+i-zakiro@users.noreply.github.com>
2023-05-26 13:56:17 +0600

    .

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div>'auto'</div>;
+  //const pathname = window.location.pathname;
+
+  return (
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/pizza/:id" element={<Pizza />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+  );
 }
 
 export default App;
