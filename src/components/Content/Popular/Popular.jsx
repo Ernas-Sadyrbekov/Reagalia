d96fd100e2057139ac0c753105491c1d90a049fe
nurdas00 <nurdas00@gmail.com>
2023-05-29 17:08:25 +0600

    .

--- src/components/Content/Popular/Popular.jsx
+++ src/components/Content/Popular/Popular.jsx
@@ -1,7 +1,23 @@
 import React from "react";
-
+import "./Popular.css";
+import ProductCard from "../ProductCard/ProductCard";
 const Popular = () => {
-  return <div></div>;
+  return (
+    <section className="main-section main_section_popular">
+      <div className="section_popular section__container">
+        <h2>Popular Products</h2>
+      </div>
+      <div className="popular_cards">
+        <ProductCard />
+      </div>
+      <div className="slider__main_btn">
+        <a href="#" tabIndex="0">
+          <span>Все Продукты</span>
+          <p></p>
+        </a>
+      </div>
+    </section>
+  );
 };
 
 export default Popular;
