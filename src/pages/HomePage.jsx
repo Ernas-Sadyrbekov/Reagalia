c9f3275be8335f60286e5f1bc7bad506299ea2b4
nurdas00 <nurdas00@gmail.com>
2023-05-31 19:40:18 +0600

    .

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,12 @@
-import { useState } from "react";
 import Header from "../components/Header/Header";
 import Carousel from "../components/Content/Carousel/Carousel";
 import Advantages from "../components/Content/Advantages/Advantages";
 import Popular from "../components/Content/Popular/Popular";
 import Slider from "../components/Content/Slider/index";
-import Modal from "../components/Content/Modal/index";
+import Modal from "../components/Content/Modal";
 import Collection from "../components/Content/Colection/index";
+import { useState } from "react";
+
 const HomePage = () => {
   const [showModal, setShowModal] = useState(false);
 
@@ -20,7 +21,6 @@ const HomePage = () => {
       <Advantages />
       <Popular toggleShowModal={toggleShowModal} />
       <Collection toggleShowModal={toggleShowModal} />
-
       <Modal show={showModal} onCloseButtonClick={toggleShowModal} />
     </div>
   );
