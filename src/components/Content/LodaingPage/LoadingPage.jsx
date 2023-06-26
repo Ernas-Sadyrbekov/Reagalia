108a9e8c41b77a5b70d39beb4d6092930eed1113
nurdas00 <nurdas00@gmail.com>
2023-06-26 19:56:17 +0600

    .

---
+++ src/components/Content/LodaingPage/LoadingPage.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./LoadingPage.css";
+const LoadingPage = () => {
+  return (
+    <div className="loading">
+      <div className="loading_page">
+        <p>l</p>
+        <p>o</p>
+        <p>a</p>
+        <p>d</p>
+        <p>i</p>
+        <p>n</p>
+        <p>g</p>
+      </div>
+    </div>
+  );
+};
+
+export default LoadingPage;
